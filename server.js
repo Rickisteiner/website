43a537d4978815c73670b2ac5782da3288526c47
Ricki Steiner <Rickisteiner@gmail.com>
2015-01-01 20:17:48 -0500

    small changes, resume add, and server works

--- server.js
+++ server.js
@@ -3,14 +3,38 @@ var http = require('http');
 
 var server = http.createServer(function(req,res){
 
-  if (req.url === "/" || req.url === "/favicon.ico" || req.url === "/start.html") {
+  if (req.url === "/" || req.url === "/favicon.ico") {
     fs.readFile("index.html", function(err,data){
       res.end(data.toString());
     });
   }
 
+  else if (req.url === '/about.html') {
+    fs.readFile("about.html", function(err,data){
+      res.end(data);
+    });
+  }
+
+  else if (req.url === '/work.html') {
+    fs.readFile("work.html", function(err,data){
+      res.end(data);
+    });
+  }
+
    else if (req.url === "/styles.css") {
     fs.readFile("styles.css", function(err,data){
+      res.end(data);
+    });
+  }
+
+  else if (req.url === '/aboutstyles.css') {
+    fs.readFile("aboutstyles.css", function(err,data){
+      res.end(data);
+    });
+  }
+
+  else if (req.url === '/workstyles.css') {
+    fs.readFile("workstyles.css", function(err,data){
       res.end(data);
     });
   }
@@ -27,35 +51,35 @@ var server = http.createServer(function(req,res){
     });
   }
 
-  else if (req.url === '/about.html') {
-    fs.readFile("about.hrml", function(err,data){
-      res.end(data);
-    });
-  }
-
-  else if (req.url === 'aboutstyles.css') {
-    fs.readFile("aboutstyles.css", function(err,data){
-      res.end(data);
-    });
-  }
-  else if (req.url === '/work.html') {
-    fs.readFile("work.hrml", function(err,data){
-      res.end(data);
-    });
-  }
-
-  else if (req.url === 'workstyles.css') {
-    fs.readFile("workstyles.css", function(err,data){
-      res.end(data);
-    });
-  }
-
   else if (req.url === '/backMain.jpg') {
     fs.readFile("back3.jpg", function(err,data){
       res.end(data);
     });
   }
 
+  else if (req.url === '/calc.png') {
+    fs.readFile("calc.png", function(err,data){
+      res.end(data);
+    });
+  }
+
+  else if (req.url === '/book.png') {
+    fs.readFile("book.png", function(err,data){
+      res.end(data);
+    });
+  }
+
+  else if (req.url === '/chat.png') {
+    fs.readFile("chat.png", function(err,data){
+      res.end(data);
+    });
+  }
+
+  else if (req.url === '/logos.png') {
+    fs.readFile("logos.png", function(err,data){
+      res.end(data);
+    });
+  }
 
 });
 
